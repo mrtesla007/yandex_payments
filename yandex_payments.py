10d70de6e8dbb232472eb2887c0922560d9c7fbc
alex <vlasyuk.alexander@gmail.com>
2018-12-05 02:45:41 +0300

    refactoring - part1

--- yandex_payments.py
+++ yandex_payments.py
@@ -1,4 +1,3 @@
-import datetime
 import time
 from yandex_checkout import Payment, Configuration
 import uuid
@@ -11,38 +10,38 @@ import config
 
 Base = declarative_base()
 
-# STATUS: pending, waiting_for_capture, succeeded, canceled
+# STATUS: pending, waiting_for_capture, succeeded, canceled, timeout
 
 # Not tested
 def check_payments(db, Kassa):
-    for instance in db.session.query(Transaction).filter(Transaction.status == 'pending'):
-        time = instance.time
-        minutes = int(time.split(":")[0]) * 60 + int(time.split(":")[0])
-        delta_time = datetime.datetime.now().hour * 60 + datetime.datetime.now().minute - minutes
-        if delta_time > config.TIME_LIMIT:
-            print('--------------------------------------------------------------------------')
-            db.delete(instance.id)
-#            Kassa.cancel(instance.transaction_id) 
+    for payment in db.get_payments(status='pending'):
+        sec_ago = time.time() - payment.time
+        if sec_ago > config.TIME_LIMIT:
+            payment.status = "timeout"
+            db.flush()
 
-    for instanse in db.session.query(Transaction).filter(Transaction.status == 'waiting_for_capture'):
-        transaction_id = Kassa.get_status(instanse.transaction_id) 
-        status = Kassa.get_status(instanse.status) 
+    for payment in db.get_payments(status='waiting_for_capture'):
+        transaction_id = payment.transaction_id
+        status = Kassa.get_status(transaction_id) 
         if status == "waiting_for_capture":
             Kassa.confirm(transaction_id)
-        if (status != instanse.status):
-            db.change_status(instanse.id, status)
+
+        if (status != payment.status):
+            payment.status = status
+            db.flush()
     
 def add_payment(db, user_id, pay_amount, return_url, description):
     payment = Kassa.send_payment(pay_amount, return_url, description)
     tr = Transaction(user_id, pay_amount, return_url, description, payment.id, payment.status)
     db.add(tr)
+    db.flush()
     return payment.confirmation.confirmation_url
     
 class DB(object):
     def __init__(self, filename):
         self.engine = create_engine(
             'sqlite:///{}'.format(filename),
-            echo=True)
+            echo=False)
     
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
@@ -56,9 +55,10 @@ class DB(object):
         elem = self.session.query(Transaction).get(id_num)
         self.session.delete(elem)
     
-    def change_status(self, id_num, status):
-        elem = self.session.query(Transaction).get(id_num)
-        elem.status = status 
+    def change_status(self, transaction, new_status):
+        if transaction.status != new_status:
+            transaction.status = new_status
+            self.flush()
         
     def flush(self):
         self.session.commit()
@@ -66,6 +66,11 @@ class DB(object):
         
     def get_all_transactions(self):
         return self.session.query(Transaction).all()
+
+    def get_payments(self, status):
+        query = self.session.query(Transaction)
+        query = query.filter(Transaction.status == status)
+        return query.all()
     
 class Transaction(Base):
     __tablename__ = "transactions"
@@ -74,17 +79,18 @@ class Transaction(Base):
     user_id = Column(String)
     pay_amount = Column(Float)
     status = Column(String)
-    time = Column(String) # Float?
+    time = Column(Float)
     
     def __init__(self, user_id, pay_amount, return_url, description, payment_id, payment_status):
         self.user_id = user_id
         self.pay_amount = pay_amount
-        self.time =  "{}:{}".format(datetime.datetime.now().hour,datetime.datetime.now().minute)
+        self.time = time.time()
         self.transaction_id = str(payment_id)
         self.status = str(payment_status)
         
     def __repr__(self):
         return "<Transcation({}, {}, {}, {}, {})>".format(self.transaction_id, self.user_id, self.pay_amount, self.status, self.time)
+
     
 class Kassa(object):
     def __init__(self, SHOP_ID, SECRET_KEY):
@@ -92,32 +98,27 @@ class Kassa(object):
     
     def send_payment(self, pay_amount, return_url, description):
         idempotence_key = str(uuid.uuid4())
-        try:
-            payment = Payment.create({
-            "amount": {
-              "value": pay_amount,
-              "currency": "RUB"
-            },
-            "confirmation": {
-              "type": "redirect",
-              "return_url": return_url
-            },
-            "description": description
-            }, idempotence_key)
-            return payment
-        except Exception as exc:
-            print("Unexpected error:", exc)        
-            return None
+        payment = Payment.create({
+        "amount": {
+          "value": pay_amount,
+          "currency": "RUB"
+        },
+        "confirmation": {
+          "type": "redirect",
+          "return_url": return_url
+        },
+        "description": description
+        }, idempotence_key)
+        return payment
+       
     
-    def is_paid(self, transaction_id):
-            payment = Payment.find_one(str(transaction_id))
-            if (payment.status == "waiting_for_capture"):
-                return True
-            return False
+    def is_paid(self, payment):
+        return payment.status == "waiting_for_capture"
+
     # Not tested
     def confirm(self, transaction_id):
         payment = Payment.find_one(str(transaction_id))
-        assert self.is_paid(transaction_id)
+        assert self.is_paid(payment)
         idempotence_key = str(uuid.uuid4())
         response = payment.capture( 
             payment.id,
@@ -129,38 +130,40 @@ class Kassa(object):
           },
           idempotence_key
         )
-        if response.status == 'succeeded':
-            return True
-        else:
-            return False
+
+        return response.status == 'succeeded'
+
+
     # Not tested
     def cancel(self, transaction_id):
         payment = Payment.find_one(str(transaction_id))
-        assert self.is_paid(transaction_id)
+        assert self.is_paid(payment)
         idempotence_key = str(uuid.uuid4())
         response = Payment.cancel( #ПРоверять результат 
           payment.id,
           idempotence_key
         )
-        if response.status == 'canceled':
-            return True
-        else:
-            return False
+        return response.status == 'canceled'
     
     # Корректность статусов всех транзакицй
     def get_status(self, transaction_id):
         payment = Payment.find_one(str(transaction_id))
         return payment.status
         
+
 if __name__=="__main__":
     db = DB("./database/test.sqlite")
     Kassa = Kassa(config.SHOP_ID, config.SECRET_KEY)
     url = add_payment(db,"Alex",1,"google.com", "description")
     print (url)
-    add_payment(db,"Sasha",1,"google.com", "description")
-    add_payment(db,"Masha",1,"google.com", "description")
-    add_payment(db,"Kolya",1,"google.com", "description")
-    add_payment(db,"Nikita",1,"google.com", "description")
-    print (db.get_all_transactions())
+    # add_payment(db,"Sasha",1,"google.com", "description")
+    # add_payment(db,"Masha",1,"google.com", "description")
+    # add_payment(db,"Kolya",1,"google.com", "description")
+    # add_payment(db,"Nikita",1,"google.com", "description")
+    for tr in db.get_all_transactions():
+        print(tr)
+
     check_payments(db, Kassa)
-    print (db.get_all_transactions())
+
+    for tr in db.get_all_transactions():
+        print(tr)
